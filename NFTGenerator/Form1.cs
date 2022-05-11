3e0411de7bca30b6d707d2ab120c0bc2a23cf8df
Corpus <programmercorpusis1@gmail.com>
2022-05-11 15:55:48 +0300

    NFT Generator class has added

--- NFTGenerator/Form1.cs
+++ NFTGenerator/Form1.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NFTGenerator
@@ -21,25 +14,23 @@ namespace NFTGenerator
             pictureBox1.MouseClick += PictureBox1_MouseClick;
         }
 
-        int historyCursor = -1;
-        List<Bitmap> history = new List<Bitmap>();
+        NFTGeneratorClass NFTGenerator = new NFTGeneratorClass();
         Random rand = new Random();
-        Bitmap lockedImage = null;
         byte takeColorMode = 0;
 
         private void button1_Click(object sender, EventArgs e)
         {
-            AddNewElement();
+            NFTGenerator.AddNewElement(pictureBox1, pictureBox2, textBox1.Text);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            OpenImage();
+            NFTGenerator.OpenImage(pictureBox1);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            SaveImage();
+            NFTGenerator.SaveImage(pictureBox2, textBox3);
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -49,22 +40,22 @@ namespace NFTGenerator
 
         private void button5_Click(object sender, EventArgs e)
         {
-            HistoryDown();
+            NFTGenerator.HistoryDown(pictureBox2);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            HistoryUp();
+            NFTGenerator.HistoryUp(pictureBox2);
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            ClearHistory();
+            NFTGenerator.ClearHistory(pictureBox2);
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            ChangeColor();
+            NFTGenerator.ChangeColor(pictureBox1, button10, textBox2);
         }
 
         private void button9_Click(object sender, EventArgs e)
@@ -79,70 +70,34 @@ namespace NFTGenerator
 
         private void button11_Click(object sender, EventArgs e)
         {
-            LockImage();
+            NFTGenerator.LockImage(pictureBox1.Image, pictureBox3);
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-            BackToLocked();
+            NFTGenerator.BackToLocked(pictureBox1);
         }
 
         private void button13_Click(object sender, EventArgs e)
         {
-            TakeRandomHat();
+            NFTGenerator.TakeRandomHat(pictureBox1);
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
-            TakeRandomBody();
-        }
-
-        private void button16_Click(object sender, EventArgs e)
-        {
-            textBox3.Text = GenerateName(7);
+            NFTGenerator.TakeRandomBody(pictureBox1);
         }
 
         private void button15_Click(object sender, EventArgs e)
         {
-            TakeRandomThing();
+            NFTGenerator.TakeRandomThing(pictureBox1);
         }
 
-        private void TakeRandomThing()
+        private void button16_Click(object sender, EventArgs e)
         {
-            int countFiles = new DirectoryInfo("things").GetFiles().Length;
-            pictureBox1.Image = Image.FromFile($"things/thing{rand.Next(countFiles)}.png");
+            textBox3.Text = NFTGenerator.GenerateName(7);
         }
-
-        private void TakeRandomBody()
-        {
-            int countFiles = new DirectoryInfo("bodies").GetFiles().Length;
-            pictureBox1.Image = Image.FromFile($"bodies/body{rand.Next(countFiles)}.png");
-        }
-
-        private void TakeRandomHat()
-        {
-            int countFiles = new DirectoryInfo("hats").GetFiles().Length;
-            pictureBox1.Image = Image.FromFile($"hats/hat{rand.Next(countFiles)}.png");
-        }
-
-        private string GenerateName(int len)
-        {
-            string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "l", "n", "p", "q", "r", "s", "sh", "zh", "t", "v", "w", "x" };
-            string[] vowels = { "a", "e", "i", "o", "u", "ae", "y" };
-            string name = "";
-            name += consonants[rand.Next(consonants.Length)].ToUpper();
-            name += vowels[rand.Next(vowels.Length)];
-            int b = 2;
-            while (b < len)
-            {
-                name += consonants[rand.Next(consonants.Length)];
-                b++;
-                name += vowels[rand.Next(vowels.Length)];
-                b++;
-            }
-            return name;
-        }
-
+        
         private void ChangeTakeColorMode(byte id, Button button)
         {
             if (takeColorMode != id)
@@ -183,245 +138,6 @@ namespace NFTGenerator
                         ChangeTakeColorMode(2, button10);
                         break;
                     }
-            }
-        }
-
-        private void BackToLocked()
-        {
-            pictureBox1.Image = lockedImage;
-        }
-        private void LockImage()
-        {
-            try
-            {
-                lockedImage = new Bitmap(pictureBox1.Image);
-                pictureBox3.Image = lockedImage;
-            }
-            catch
-            {
-                MessageBox.Show("Отсутствует изображение!", "Ошибка фиксации", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-
-        private void ChangeColor()
-        {
-            if (pictureBox1.Image != null)
-            {
-                Color newColor = Color.FromArgb(rand.Next(255), rand.Next(255), rand.Next(255));
-                Color changeColor;
-                Bitmap addImage = new Bitmap(pictureBox1.Image);
-                try
-                {
-                    var colorChannels = textBox2.Text.Split(',');
-                    changeColor = Color.FromArgb(int.Parse(colorChannels[0]), int.Parse(colorChannels[1]), int.Parse(colorChannels[2]));
-                }
-                catch
-                {
-                    MessageBox.Show("Ошибка изменения изображения!\nПроверьте правильность ввода цветовых каналов!\nЦвет установлен в цвет по умолчанию!", "Ошибка изменения", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    changeColor = Color.FromArgb(34, 32, 52);
-                    textBox1.Text = "34,32,52";
-                }
-                for (int i = 0; i < pictureBox1.Image.Width; ++i)
-                {
-                    for (int j = 0; j < pictureBox1.Image.Height; ++j)
-                    {
-                        if (addImage.GetPixel(i, j) == changeColor)
-                        {
-                            addImage.SetPixel(i, j, newColor);
-                        }
-                    }
-                }
-                pictureBox1.Image = addImage;
-                textBox2.Text = $"{newColor.R},{newColor.G},{newColor.B}";
-                button10.BackColor = newColor;
-            }
-        }
-
-        private void HistoryUp()
-        {
-            if (historyCursor + 1 < history.Count)
-            {
-                historyCursor++;
-                int bufCursor = -1;
-                foreach (var item in history)
-                {
-                    bufCursor++;
-                    if (bufCursor == historyCursor)
-                    {
-                        pictureBox2.Image = item;
-                        break;
-                    }
-                }
-            }
-        }
-
-        private void ClearHistory()
-        {
-            pictureBox2.Image = null;
-            history = new List<Bitmap>();
-            historyCursor = -1;
-        }
-
-        private void HistoryDown()
-        {
-            if (historyCursor - 1 >= 0)
-            {
-                historyCursor--;
-                int bufCursor = -1;
-                foreach (var item in history)
-                {
-                    bufCursor++;
-                    if (bufCursor == historyCursor)
-                    {
-                        pictureBox2.Image = item;
-                        break;
-                    }
-                }
-            }
-        }
-
-        private void fillImageBackground()
-        {
-            Bitmap resultImage = new Bitmap(pictureBox2.Image);
-            for (int i = 0; i < pictureBox2.Image.Width; ++i)
-            {
-                for (int j = 0; j < pictureBox2.Image.Height; ++j)
-                {
-                    if (resultImage.GetPixel(i, j) == Color.FromArgb(0, 0, 0, 0))
-                    {
-                        resultImage.SetPixel(i, j, pictureBox2.BackColor);
-                    }
-                }
-            }
-            pictureBox2.Image = resultImage;
-        }
-
-        private void OpenImage()
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "Image Files(*.PNG;*.JPG;*.GIF;*.BMP)|*.PNG;*.JPG;*.GIF;*.BMP|All files (*.*)|*.*";
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                try
-                {
-                    pictureBox1.Image = new Bitmap(ofd.FileName);
-                }
-                catch
-                {
-                    MessageBox.Show("Ошибка загрузки изображения!", "Ошибка загрузки", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-        }
-
-        private void SaveImage()
-        {
-            if (pictureBox2.Image != null)
-            {
-                SaveFileDialog sfd = new SaveFileDialog();
-                sfd.Title = "Сохранить изображение как...";
-                sfd.OverwritePrompt = true;
-                sfd.CheckPathExists = true;
-                sfd.ShowHelp = true;
-                sfd.Filter = "Image Files(*.PNG)|*.PNG|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image Files(*.BMP)|*.BMP|All files (*.*)|*.*";
-                if (sfd.ShowDialog() == DialogResult.OK)
-                {
-                    try
-                    {
-                        fillImageBackground();
-                        AddName();
-                        pictureBox2.Image.Save(sfd.FileName);
-                        MessageBox.Show("Изображение успешно сохранено!", "Сохранение успешно", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
-
-                    }
-                    catch
-                    {
-                        MessageBox.Show("Ошибка сохранения изображения!", "Ошибка сохранения", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                }
-            }
-        }
-
-        private void AddName()
-        {
-            string name = textBox3.Text;
-            Graphics gr = Graphics.FromImage(pictureBox2.Image);
-            Image img = pictureBox2.Image;
-            Font font = new Font(new FontFamily("Courier"), 25, FontStyle.Bold);
-            gr.DrawString(name, font, new SolidBrush(Color.Black), 12, 12);
-            gr.DrawString(name, font, new SolidBrush(Color.White), 10, 10);
-            AddToHistory(new Bitmap(img, img.Width, img.Height));
-        }
-
-        private void AddToHistory(Bitmap newBitmap)
-        {
-            if (history.Count > historyCursor + 1)
-            {
-                int newHistoryCursor = -1;
-                List<Bitmap> newHistory = new List<Bitmap>();
-                foreach (var item in history)
-                {
-                    if (historyCursor > newHistoryCursor)
-                    {
-                        newHistory.Add(item);
-                        newHistoryCursor++;
-                    }
-                }
-                newHistory.Add(new Bitmap(pictureBox2.Image));
-                newHistoryCursor++;
-                history = new List<Bitmap>();
-                foreach (var item in newHistory)
-                    history.Add(item);
-                historyCursor = newHistoryCursor;
-            }
-            else
-            {
-                history.Add(new Bitmap(pictureBox2.Image));
-                historyCursor++;
-            }
-        }
-
-        private void AddNewElement()
-        {
-            if (pictureBox1.Image != null)
-            {
-                try
-                {
-                    Bitmap addImage = new Bitmap(pictureBox1.Image);
-                    Bitmap resultImage;
-                    if (pictureBox2.Image != null)
-                        resultImage = new Bitmap(pictureBox2.Image);
-                    else
-                        resultImage = new Bitmap(pictureBox1.Image.Width, pictureBox1.Image.Height);
-                    Color ignoreColor;
-                    try
-                    {
-                        var colorChannels = textBox1.Text.Split(',');
-                        ignoreColor = Color.FromArgb(int.Parse(colorChannels[0]), int.Parse(colorChannels[1]), int.Parse(colorChannels[2]));
-                    }
-                    catch
-                    {
-                        MessageBox.Show("Ошибка изменения изображения!\nПроверьте правильность ввода цветовых каналов!\nЦвет установлен в цвет по умолчанию!", "Ошибка изменения", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        ignoreColor = Color.FromArgb(0, 255, 0);
-                        textBox1.Text = "0,255,0";
-                    }
-                    for (int i = 0; i < pictureBox1.Image.Width; ++i)
-                    {
-                        for (int j = 0; j < pictureBox1.Image.Height; ++j)
-                        {
-                            if (addImage.GetPixel(i, j) != ignoreColor)
-                            {
-                                resultImage.SetPixel(i, j, addImage.GetPixel(i, j));
-                            }
-                        }
-                    }
-                    pictureBox2.Image = resultImage;
-                    AddToHistory(resultImage);
-                }
-                catch
-                {
-                    MessageBox.Show("Ошибка изменения изображения!\nПроверьте является ли добавляемое изображение меньшим или одного размера с финальным!", "Ошибка изменения", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
             }
         }
 
---
+++ NFTGenerator/NFTGeneratorClass.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NFTGenerator
+{
+    public class NFTGeneratorClass
+    {
+        private Random _rand;
+        private int _historyCursor;
+        private List<Bitmap> _history;
+        private Bitmap _lockedImage;
+
+        public NFTGeneratorClass()
+        {
+            _rand = new Random();
+            _historyCursor = -1;
+            _history = new List<Bitmap>();
+            _lockedImage = null;
+        }
+
+        public void AddNewElement(PictureBox pictureBox, PictureBox resultPictureBox, string maskColor)
+        {
+            if (pictureBox.Image != null)
+            {
+                try
+                {
+                    Bitmap addImage = new Bitmap(pictureBox.Image);
+                    Bitmap resultImage;
+                    if (resultPictureBox.Image != null)
+                        resultImage = new Bitmap(resultPictureBox.Image);
+                    else
+                        resultImage = new Bitmap(pictureBox.Image.Width, pictureBox.Image.Height);
+                    Color ignoreColor;
+                    try
+                    {
+                        var colorChannels = maskColor.Split(',');
+                        ignoreColor = Color.FromArgb(int.Parse(colorChannels[0]), int.Parse(colorChannels[1]), int.Parse(colorChannels[2]));
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Ошибка изменения изображения!\nПроверьте правильность ввода цветовых каналов!\nВыбран цвет маски по умолчанию!", "Ошибка изменения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        ignoreColor = Color.FromArgb(0, 255, 0);
+                    }
+                    for (int i = 0; i < pictureBox.Image.Width; ++i)
+                    {
+                        for (int j = 0; j < pictureBox.Image.Height; ++j)
+                        {
+                            if (addImage.GetPixel(i, j) != ignoreColor)
+                            {
+                                resultImage.SetPixel(i, j, addImage.GetPixel(i, j));
+                            }
+                        }
+                    }
+                    resultPictureBox.Image = resultImage;
+                    AddToHistory(resultImage);
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка изменения изображения!\nПроверьте является ли добавляемое изображение меньшим или одного размера с финальным!", "Ошибка изменения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        public void AddToHistory(Bitmap newBitmap)
+        {
+            if (_history.Count > _historyCursor + 1)
+            {
+                int newHistoryCursor = -1;
+                List<Bitmap> newHistory = new List<Bitmap>();
+                foreach (var item in _history)
+                {
+                    if (_historyCursor > newHistoryCursor)
+                    {
+                        newHistory.Add(item);
+                        newHistoryCursor++;
+                    }
+                }
+                newHistory.Add(new Bitmap(newBitmap));
+                newHistoryCursor++;
+                _history = new List<Bitmap>();
+                foreach (var item in newHistory)
+                    _history.Add(item);
+                _historyCursor = newHistoryCursor;
+            }
+            else
+            {
+                _history.Add(new Bitmap(newBitmap));
+                _historyCursor++;
+            }
+        }
+
+        public void TakeRandomThing(PictureBox picture)
+        {
+            int countFiles = new DirectoryInfo("things").GetFiles().Length;
+            picture.Image = Image.FromFile($"things/thing{_rand.Next(countFiles)}.png");
+        }
+
+        public void TakeRandomBody(PictureBox picture)
+        {
+            int countFiles = new DirectoryInfo("bodies").GetFiles().Length;
+            picture.Image = Image.FromFile($"bodies/body{_rand.Next(countFiles)}.png");
+        }
+
+        public void TakeRandomHat(PictureBox picture)
+        {
+            int countFiles = new DirectoryInfo("hats").GetFiles().Length;
+            picture.Image = Image.FromFile($"hats/hat{_rand.Next(countFiles)}.png");
+        }
+
+        public string GenerateName(int len)
+        {
+            string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "l", "n", "p", "q", "r", "s", "sh", "zh", "t", "v", "w", "x" };
+            string[] vowels = { "a", "e", "i", "o", "u", "ae", "y" };
+            string name = "";
+            name += consonants[_rand.Next(consonants.Length)].ToUpper();
+            name += vowels[_rand.Next(vowels.Length)];
+            int b = 2;
+            while (b < len)
+            {
+                name += consonants[_rand.Next(consonants.Length)];
+                b++;
+                name += vowels[_rand.Next(vowels.Length)];
+                b++;
+            }
+            return name;
+        }
+
+        public void BackToLocked(PictureBox picture)
+        {
+            picture.Image = _lockedImage;
+        }
+        public void LockImage(Image image, PictureBox bufPictureBox)
+        {
+            try
+            {
+                _lockedImage = new Bitmap(image);
+                bufPictureBox.Image = _lockedImage;
+            }
+            catch
+            {
+                MessageBox.Show("Отсутствует изображение!", "Ошибка фиксации", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void ChangeColor(PictureBox pictureBox, Button button, TextBox colorTextBox)
+        {
+            if (pictureBox.Image != null)
+            {
+                Color newColor = Color.FromArgb(_rand.Next(255), _rand.Next(255), _rand.Next(255));
+                Color changeColor;
+                Bitmap addImage = new Bitmap(pictureBox.Image);
+                try
+                {
+                    var colorChannels = colorTextBox.Text.Split(',');
+                    changeColor = Color.FromArgb(int.Parse(colorChannels[0]), int.Parse(colorChannels[1]), int.Parse(colorChannels[2]));
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка изменения изображения!\nПроверьте правильность ввода цветовых каналов!\nЦвет установлен в цвет по умолчанию!", "Ошибка изменения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                for (int i = 0; i < pictureBox.Image.Width; ++i)
+                {
+                    for (int j = 0; j < pictureBox.Image.Height; ++j)
+                    {
+                        if (addImage.GetPixel(i, j) == changeColor)
+                        {
+                            addImage.SetPixel(i, j, newColor);
+                        }
+                    }
+                }
+                pictureBox.Image = addImage;
+                colorTextBox.Text = $"{newColor.R},{newColor.G},{newColor.B}";
+                button.BackColor = newColor;
+            }
+        }
+
+        public void HistoryUp(PictureBox pictureBox)
+        {
+            if (_historyCursor + 1 < _history.Count)
+            {
+                _historyCursor++;
+                int bufCursor = -1;
+                foreach (var item in _history)
+                {
+                    bufCursor++;
+                    if (bufCursor == _historyCursor)
+                    {
+                        pictureBox.Image = item;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void HistoryDown(PictureBox pictureBox)
+        {
+            if (_historyCursor - 1 >= 0)
+            {
+                _historyCursor--;
+                int bufCursor = -1;
+                foreach (var item in _history)
+                {
+                    bufCursor++;
+                    if (bufCursor == _historyCursor)
+                    {
+                        pictureBox.Image = item;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void ClearHistory(PictureBox pictureBox)
+        {
+            pictureBox.Image = null;
+            _history = new List<Bitmap>();
+            _historyCursor = -1;
+        }
+
+        public void fillImageBackground(PictureBox pictureBox)
+        {
+            Bitmap resultImage = new Bitmap(pictureBox.Image);
+            for (int i = 0; i < pictureBox.Image.Width; ++i)
+            {
+                for (int j = 0; j < pictureBox.Image.Height; ++j)
+                {
+                    if (resultImage.GetPixel(i, j) == Color.FromArgb(0, 0, 0, 0))
+                    {
+                        resultImage.SetPixel(i, j, pictureBox.BackColor);
+                    }
+                }
+            }
+            pictureBox.Image = resultImage;
+        }
+
+        public void OpenImage(PictureBox pictureBox)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files(*.PNG;*.JPG;*.GIF;*.BMP)|*.PNG;*.JPG;*.GIF;*.BMP|All files (*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox.Image = new Bitmap(ofd.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка загрузки изображения!", "Ошибка загрузки", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        public void SaveImage(PictureBox pictureBox, TextBox nameTextBox)
+        {
+            if (pictureBox.Image != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Сохранить изображение как...";
+                sfd.OverwritePrompt = true;
+                sfd.CheckPathExists = true;
+                sfd.ShowHelp = true;
+                sfd.Filter = "Image Files(*.PNG)|*.PNG|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image Files(*.BMP)|*.BMP|All files (*.*)|*.*";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        fillImageBackground(pictureBox);
+                        AddName(nameTextBox, pictureBox);
+                        pictureBox.Image.Save(sfd.FileName);
+                        MessageBox.Show("Изображение успешно сохранено!", "Сохранение успешно", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Ошибка сохранения изображения!", "Ошибка сохранения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        public void AddName(TextBox nameTextBox, PictureBox pictureBox)
+        {
+            string name = nameTextBox.Text;
+            Graphics gr = Graphics.FromImage(pictureBox.Image);
+            Image img = pictureBox.Image;
+            Font font = new Font(new FontFamily("Courier"), 25, FontStyle.Bold);
+            gr.DrawString(name, font, new SolidBrush(Color.Black), 12, 12);
+            gr.DrawString(name, font, new SolidBrush(Color.White), 10, 10);
+            AddToHistory(new Bitmap(img, img.Width, img.Height));
+        }
+    }
+}
